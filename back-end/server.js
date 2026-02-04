bd4ee124bfeb914e841bb6b166a42597b2b6136c
Muhammed Faisal R <faisalrasheed.pro@gmail.com>
2026-02-04 18:39:31 +0000

    Update backend response text

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
-const app=express()
-const port=process.env.PORT || 3000;
-const container=process.env.CONTAINER || "Container"
-app.get('/',(req,res)=>{
-    res.json({message:`Hello this is  ${container} running on ${port}`})
-})
-app.get('/health',(req,res)=>{
-    res.status(200).json({status:"ok",container})
-})
-app.listen(port,()=>{
-    console.log(`${container} started on port ${port}`)
-})
+
+const app = express();
+const port = process.env.PORT || 3000;
+const container = process.env.CONTAINER || "Container";
+
+app.get('/', (req, res) => {
+    res.send(`Load Balancer Backend | ${container} | Port ${port}`);
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: "ok", container });
+});
+
+app.listen(port, () => {
+    console.log(`${container} started on port ${port}`);
+});
